a849f3b67159c6b5a0499bd07cc3aaf3e7ba9a6b
kirkconnell <carlos.kirkconnell@gmail.com>
2010-01-23 15:17:01 -0600

    creating sample builder class and spec frame

---
+++ lib/sample_builder.rb
@@ -0,0 +1,3 @@
+class SampleBuilder
+  
+end
---
+++ spec/lib/sample_builder_spec.rb
@@ -0,0 +1,39 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe SampleBuilder do
+  it "should build a scalar sample" do
+    flunk
+  end
+  
+  it "should build a string sample" do
+    flunk
+  end
+  
+  it "should build a big string sample" do
+    flunk
+  end
+  
+  it "should build a scalar array sample" do
+    flunk
+  end
+  
+  it "should build a string array sample" do
+    flunk
+  end
+  
+  it "should build a big string array sample" do
+    flunk
+  end
+  
+  it "should build a scalar big array sample" do
+    flunk
+  end
+  
+  it "should build a string big array sample" do
+    flunk
+  end
+  
+  it "should build a big string big array sample" do
+    flunk
+  end
+end
